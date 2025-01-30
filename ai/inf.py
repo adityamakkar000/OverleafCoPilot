78decac06ada871f1a3a2c1c3e4934323dcc0f4e
adityamakkar000 <aditymakkar000@gmail.com>
2025-01-29 22:45:22 -0500

    new things

--- ai/inf.py
+++ ai/inf.py
@@ -5,15 +5,18 @@ import hydra
 from hydra.core.config_store import ConfigStore
 from omegaconf import OmegaConf, MISSING
 from dataclasses import dataclass
-
+from ft import ftConfig
 
 @dataclass
 class InferenceConfig:
     path: str = MISSING
 
 class ModelManager:
-    def __init__(self):
+    def __init__(self, cfg: ftConfig):
         self.device = self._get_device()
+        self.model, self.tokenizer = self.load_model(cfg)
+        self.device = self._get_device()
+        self.instruction = cfg.dataCFG.instruction
 
     @staticmethod
     def _get_device():
@@ -24,7 +27,9 @@ class ModelManager:
         return "cpu"
 
     def load_model(self, cfg: InferenceConfig):
-        model = AutoModelForCausalLM.from_pretrained(cfg.model_id, device_map=self.device)
+        model = AutoModelForCausalLM.from_pretrained(cfg.model_id,
+                                                     torch_dtype=torch.bfloat16,
+                                                     device_map=self.device)
         merged_model = PeftModel.from_pretrained(
             model, f"{cfg.output}/{cfg.name}/finetuned_models/"
         )
@@ -34,53 +39,46 @@ class ModelManager:
         )
         return merged_model, tokenizer
 
-    def get_completion(self, query: str, model, tokenizer) -> str:
-        prompt_template = """
-        <start_of_turn>user
-        Below is an instruction that describes a task. Write a response that appropriately completes the request.
-        {query}
-        <end_of_turn>\n<start_of_turn>model
-        """
-        prompt = prompt_template.format(query=query)
-        tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.padding_side = "left"
+    def generate_prompt(self, input: str) -> str:
+        message = [{
+            "role": "user",
+            "content": f"{self.instruction} {input}"
+        }]
 
-        encodeds = tokenizer(prompt, return_tensors="pt", add_special_tokens=True)
-        model_inputs = encodeds.to(self.device)
+        return self.tokenizer.apply_chat_template(message, add_generation_prompt=True, return_tensors="pt")
 
-        generated_ids = model.generate(
-            **model_inputs,
-            max_new_tokens=50,
+    def __call__(self, input: str) -> str:
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.tokenizer.padding_side = "left"
+
+        input_tensor = self.generate_prompt(input)
+        outputs = self.model.generate(
+            input_tensor.to(self.device),
+            max_new_tokens=100,
             do_sample=True,
-            pad_token_id=tokenizer.eos_token_id,
+            pad_token_id=self.tokenizer.eos_token_id
         )
-        return tokenizer.decode(generated_ids[0], skip_special_tokens=True)
 
+        text = self.tokenizer.decode(outputs[0][input_tensor.shape[1]:], skip_special_tokens=True)
+        return text
 
 def setup_config_store():
     cs = ConfigStore.instance()
     cs.store(name="inference_config", node=InferenceConfig)
-
 
 @hydra.main(version_base=None, config_path="./configs", config_name="inference")
 def main(cfg: InferenceConfig):
     cfg = OmegaConf.load(f"{cfg.path}/config.yaml")
     print(OmegaConf.to_yaml(cfg))
 
-    model_manager = ModelManager()
-    model, tokenizer = model_manager.load_model(cfg)
-
-    instruction = cfg.dataCFG.instruction
-
+    model_manager = ModelManager(cfg)
 
     prompt_input = input("Enter prompt: ")
     while prompt_input != "exit":
-        prompt = r"""{}""".format(prompt_input)
-        query = f"{instruction} {prompt}"
-
-        result = model_manager.get_completion(query=query, model=model, tokenizer=tokenizer)
+        result = model_manager(
+            prompt_input
+        )
         print(result)
-
         prompt_input = input("Enter prompt: ")
 
 
